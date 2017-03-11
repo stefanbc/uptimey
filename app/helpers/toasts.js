cf5a243f5ea431c315bd865c22b7465cc00efbbe
stefanbc <stefcosma@gmail.com>
2017-03-11 18:50:37 +0200

    Fixes toasts issues

--- app/helpers/toasts.js
+++ app/helpers/toasts.js
@@ -7,8 +7,14 @@ const _ = require('lodash');
 module.exports = {
 
     wormHole: $('.container .toasts-wormhole'),
-    defaultPosition: 'tc',
+    defaultPosition: 'tr',
 
+    /**
+     * The main method for generating a toast
+     * @param {String} type
+     * @param {String} msg
+     * @param {String} position
+     */
     init(type, msg, position = this.defaultPosition) {
         _.bindAll(this);
 
@@ -16,39 +22,66 @@ module.exports = {
         this[type](msg, position);
 
         _.delay(() => {
-            this.clearAll();
-        }, 10000);
+            this.hide();
+        }, 6000);
     },
 
+    /**
+     * Generate a success toast
+     * @param {String} msg
+     * @param {String} position
+     */
     success(msg, position) {
         let toast = this.buildToast('success', msg, position);
 
         this.wormHole.append(toast);
     },
 
+    /**
+     * Generate a warning toast
+     * @param {String} msg
+     * @param {String} position
+     */
     warning(msg, position) {
         let toast = this.buildToast('warning', msg, position);
 
         this.wormHole.append(toast);
     },
 
+    /**
+     * Generate an error toast
+     * @param {String} msg
+     * @param {String} position
+     */
     error(msg, position) {
-        let toast = this.buildToast('error', msg, position);
+        let toast = this.buildToast('danger', msg, position);
 
         this.wormHole.append(toast);
     },
 
+    /**
+     * Build a toast and animate it
+     * @param {String} type
+     * @param {String} msg
+     * @param {String} position
+     */
     buildToast(type, msg, position) {
         let toast = `<div class="toast toast-${type} toast-${position} animated fadeInDown">${msg}</div>`;
 
         return toast;
     },
 
+    /**
+     * Clears all toasts in the wormhole
+     */
     clearAll() {
-        this.wormHole.find('.toast').removeClass('fadeInDown').addClass('fadeInOut');
+        this.wormHole.find('.toast').remove();
+    },
 
-        _.delay(() => {
-            this.wormHole.find('.toast').remove();
-        }, 2000);
+    /**
+     * Hides a toast in the wormhole
+     */
+    hide() {
+        this.wormHole.find('.toast').removeClass('fadeInDown').addClass('fadeOutUp');
     }
 };
